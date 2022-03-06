5974bf02415dac2d175a9eeab72bf6b4c10b5b34
lvqichao <1148483029@qq.com>
2022-03-06 16:38:36 +0800

    第一次提交

--- community/src/main/java/com/nowcoder/community/entity/DiscussPost.java
+++ community/src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -2,19 +2,21 @@ package com.nowcoder.community.entity;
 
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 
 @Data
 @ToString
+@Repository
 public class DiscussPost {
 
     private int id;
     private int userId;
     private String title;
     private String content;
-    private int type;
-    private int status;
+    private int type; // 1表示置顶
+    private int status; // 1表示精华
     private Date createTime;
     private int commentCount;
     private double score;
--- community/src/test/java/com/nowcoder/community/MapperTest.java
+++ community/src/test/java/com/nowcoder/community/MapperTest.java
@@ -1,7 +1,9 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +20,18 @@ public class MapperTest {
     @Autowired
     DiscussPostMapper discussPostMapper;
 
+    @Autowired
+    UserMapper userMapper;
     @Test
     public void discussPostMapperTest(){
         List<DiscussPost> discussPosts = discussPostMapper.selectDiscussPosts(0, 1, 10);
         System.out.println(discussPosts);
+        System.out.println(discussPostMapper.selectDiscussPostRows(0));
+    }
+
+    @Test
+    public void UserMapperTest(){
+        User user = userMapper.selectById(1);
+        System.out.println(user);
     }
 }
