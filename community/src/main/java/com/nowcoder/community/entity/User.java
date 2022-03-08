e95b264d335016cc855f11954674460a2cebc228
lvqichao <1148483029@qq.com>
2022-03-08 18:29:32 +0800

    community-2.5 实现登录和退出

--- community/src/main/java/com/nowcoder/community/entity/User.java
+++ community/src/main/java/com/nowcoder/community/entity/User.java
@@ -16,7 +16,7 @@ public class User {
     private String salt;
     private String email;
     private int type;
-    private int status;
+    private int status; // 0 未激活 1 已激活
     private String activationCode;
     private String headerUrl;
     private Date createTime;
