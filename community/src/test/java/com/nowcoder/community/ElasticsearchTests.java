a8eb137e831771848af2fea9b904bad2be342d5a
lvqichao <1148483029@qq.com>
2022-03-17 16:25:17 +0800

    community-6.3 实现开发社区搜索功能

--- community/src/test/java/com/nowcoder/community/ElasticsearchTests.java
+++ community/src/test/java/com/nowcoder/community/ElasticsearchTests.java
@@ -40,6 +40,19 @@ public class ElasticsearchTests {
     }
 
     @Test
+    public void testInsertList() {
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(101, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(102, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(103, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(111, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(112, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(131, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(132, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(133, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(134, 0, 100));
+    }
+
+    @Test
     public void testUpdate() {
         DiscussPost post = discussMapper.selectDiscussPostById(241);
         post.setContent("我是新人,使劲灌水.");
