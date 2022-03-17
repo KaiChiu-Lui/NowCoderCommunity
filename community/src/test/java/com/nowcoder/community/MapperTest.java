9548b692bbb5411a5bb64e6c4cc8cd9def89e527
lvqichao <1148483029@qq.com>
2022-03-17 10:16:08 +0800

    community-5.5 实现显示系统通知

--- community/src/test/java/com/nowcoder/community/MapperTest.java
+++ community/src/test/java/com/nowcoder/community/MapperTest.java
@@ -3,7 +3,11 @@ package com.nowcoder.community;
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Message;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.MessageService;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.HostHolder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +20,18 @@ import java.util.List;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = CommunityApplication.class)
-public class MapperTest {
+public class MapperTest implements CommunityConstant {
     @Autowired
     DiscussPostMapper discussPostMapper;
 
     @Autowired
     UserMapper userMapper;
+
+    @Autowired
+    MessageService messageService;
+
+    @Autowired
+    private HostHolder hostHolder;
 
     @Test
     public void discussPostMapperTest(){
@@ -34,6 +44,15 @@ public class MapperTest {
     public void UserMapperTest(){
         User user = userMapper.selectById(1);
         System.out.println(user);
+    }
+
+    @Test
+    public void NoticeTest(){
+        Message latestNotice = messageService.findLatestNotice(152, TOPIC_COMMENT);
+        if(latestNotice==null){
+            System.out.println("message查找结果输出为空");
+        }
+        System.out.println(latestNotice);
     }
 
 }
